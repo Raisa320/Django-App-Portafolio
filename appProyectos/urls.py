5679d0c1e94f01283ff482c0465561de4003160d
Raisa320 <67403486+Raisa320@users.noreply.github.com>
2022-12-08 00:46:25 -0500

    mostrar tabla de proyectos

--- appProyectos/urls.py
+++ appProyectos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from .views import index,ProyectoCreate
 
 urlpatterns = [
-    path('register/',ProyectoCreate.as_view() , name='proyecto-register'),
+    path('register/',login_required(ProyectoCreate.as_view()) , name='proyecto-register'),
 ]
--- appProyectos/views.py
+++ appProyectos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import  redirect, render
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import CreateView
 from django.contrib.auth.decorators import login_required
 from appProyectos.forms import  ProyectoForm
@@ -8,16 +9,21 @@ from appProyectos.models import Proyecto
 # Create your views here.
 @login_required
 def index(request):
-    context={}
+    current_user=request.user
+    context={
+        "proyectos": current_user.proyecto_set.all(),
+        "cantidad":current_user.proyecto_set.count()
+    }
+
     return render(request,"proyecto/index.html",context)
     
 
-class ProyectoCreate(CreateView):
+class ProyectoCreate(SuccessMessageMixin,CreateView):
     form_class=ProyectoForm
     template_name = 'proyecto/form-proyecto.html'
     success_url = '/'
-    
+    success_message = "Se ha registrado tu proyecto correctamente"
+
     def form_valid(self,form):
-        print("hola")
         form.instance.autor=self.request.user
         return super().form_valid(form)
